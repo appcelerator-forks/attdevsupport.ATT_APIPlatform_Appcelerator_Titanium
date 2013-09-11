2e5c26282c6ac25372c40fe2c9661fb5b75260c4
attdevsupport <developer.support@att.com>
2013-09-11 13:48:23 -0700

    Release 3.0 OAuth Updates & Text To Speech Support

---
+++ SampleApps/KitchenSink/KitchenSinkClient/Resources/speechToText.js
@@ -0,0 +1,193 @@
+"use strict";
+
+// Att common JS module needs to be required.
+var attAPIs = require('att');
+
+// Note: Application Developer should get access key and secret key by
+// registering on www.developer.att.com website
+// and creating sample Application.
+
+var accessKey = Ti.App.Properties.getString('accessKey');
+var secretKey = Ti.App.Properties.getString('secretKey');
+var scope = Ti.App.Properties.getString('scope');
+var grantType = 'client_credentials';
+
+// pass accessKey and secretKey for authorization
+attAPIs.ATT.authorize(accessKey, secretKey, scope, grantType);
+
+//Load helper functions
+var util = require('util'),
+    forEach = util.forEach,
+    copyObjTo = util.copyObjTo,
+    createPicker = util.UI.createPicker,
+    deviceState = util.state.device,
+    openPopUp = util.UI.createResponseWindow();
+    
+var sttConstants = {
+    margin: 10,
+    padding: 10,
+    buttonWidth: '95%'
+};
+
+util.UI.createSpacer.defaultSpacerHeight = sttConstants.margin;
+
+//creating a View to contain the API functionality
+var mainWindow = Ti.UI.currentWindow;
+
+mainWindow.addEventListener('android:back', function(e) {
+    mainWindow.close();
+});
+
+var scrollView = Ti.UI.createScrollView({
+    top: 0,
+    layout:'vertical',
+    height: Ti.UI.SIZE,
+    left: sttConstants.margin,
+    right: sttConstants.margin
+});
+
+var fileName = Ti.UI.createLabel({
+    top: sttConstants.margin,
+    height: Ti.UI.SIZE,
+    color: 'black'
+});
+scrollView.add(fileName);
+
+function clearFilePath() {
+    filePath = null;
+    fileName.text = 'File Name : No File Taken';
+}
+clearFilePath();
+
+var fileChooseView = Ti.UI.createView({
+	top: sttConstants.margin,
+	width : '100%',
+	height: Ti.UI.SIZE,
+	borderColor : 'black',
+	backgroundColor : 'aqua',
+	layout:'vertical'
+});
+
+var fileChoose = Ti.UI.createButton({
+    title : 'Choose file',
+    top: sttConstants.margin,
+    width: sttConstants.buttonWidth
+});
+
+var filePath, mysound,
+    folderPath = Ti.Filesystem[deviceState.is.android ? 'externalStorageDirectory' : 'applicationDataDirectory'];
+
+fileChoose.addEventListener('click', function() {
+    util.UI.createGetFilePathWindow(folderPath, function(fileData) {
+        filePath = fileData.path;
+        mysound = Ti.Media.createSound({ url : filePath });
+        fileName.text = 'File Name : ' + fileData.name;
+    }, function(error) {
+        alert(error.message);
+    });
+});
+// If Play button is clicked without choosing a file then it will throw an exception.
+var audioPlayer = Ti.UI.createButton({
+    title : 'Play',
+    top: sttConstants.margin,
+    width: sttConstants.buttonWidth
+});
+audioPlayer.addEventListener('click', function() {
+    if(mysound) {
+        mysound.play();
+    } else {
+        alert('No sound selected.  Please select a file' + (deviceState.is.android ? '.' : ' or record your speech.'));
+    }
+});
+
+fileChooseView.add(fileChoose);
+fileChooseView.add(audioPlayer);
+fileChooseView.add(util.UI.createSpacer(sttConstants.margin));
+scrollView.add(fileChooseView);
+
+if (!deviceState.is.android) {
+    var orLabel = Ti.UI.createLabel({
+        top: sttConstants.margin,
+        text : 'OR',
+        width : '100%',
+        textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+    });
+    scrollView.add(orLabel);
+    
+    var recorder = Ti.UI.createButton({
+        top: sttConstants.margin,
+        title : 'Voice Record',
+        width: sttConstants.buttonWidth
+    });
+    recorder.addEventListener('click', function() {
+        Ti.API.info('Recorder Button Clicked.');
+        clearFilePath();
+        var win = Titanium.UI.createWindow({
+            modal : true,
+            url : 'audioRecorder.js',
+            title : "Recorder Screen",
+            backgroundColor : '#fff'
+        });
+        var recorderClose = Ti.UI.createButton({
+            style : Ti.UI.iPhone.SystemButtonStyle.DONE,
+            title : 'close'
+        });
+        
+        recorderClose.addEventListener('click', function() {
+            Ti.API.info('Recording Screen Closed.');
+            win.close();
+        });
+        
+        win.setRightNavButton(recorderClose);
+        win.open();
+    });
+    scrollView.add(recorder);
+}
+
+var speechContextPickerView = createPicker({ 
+    width: sttConstants.buttonWidth,
+    borderColor : 'black'
+}, ['', 'Generic', 'BusinessSearch', 'Websearch', 'SMS', 'Voicemail', 'QuestionAndAnswer', 'Gaming', 'SocialMedia']);
+scrollView.add(util.UI.createLabelView('Speech Context', speechContextPickerView, { top: sttConstants.margin }));
+
+var languagePickerView = createPicker({ 
+    width: sttConstants.buttonWidth,
+    borderColor : 'black'
+}, ['', 'en-US', 'es-US']);
+scrollView.add(util.UI.createLabelView('Content Language', languagePickerView));
+
+var baseArgument = {
+	'contentType' : 'audio/wav',
+	'accept' : 'application/xml'
+};
+
+var speechButton = Ti.UI.createButton({
+    title : 'Speech',
+    top: 20,
+    width: sttConstants.buttonWidth,
+    height: Ti.UI.SIZE
+});
+speechButton.addEventListener('click', function() {"use strict";
+	Ti.API.info('Speech Button Clicked.');
+	
+	var argument = copyObjTo({
+	   contentLanguage: languagePickerView.value || '',
+	   xSpeechContext: speechContextPickerView.value || '',
+	   filePath: util.formatFilePath(filePath)
+	}, baseArgument);
+	
+	Ti.API.info(argument.filePath);
+    
+	attAPIs.ATT.Speech.speechToText(argument, function(data) {
+		Ti.API.info('Success Callback:' + ((argument.accept === 'application/xml') ? data : JSON.stringify(data)));
+		openPopUp(data);
+	}, function(error) {
+		Ti.API.error('Error Callback: ' + error);
+		openPopUp(error, true);
+	});
+});
+
+scrollView.add(speechButton);
+scrollView.add(util.UI.createSpacer());
+
+mainWindow.add(scrollView);
---
+++ SampleApps/KitchenSink/KitchenSinkClient/Resources/textToSpeech.js
@@ -0,0 +1,161 @@
+//Att common JS module needs to be required.
+var attAPIs = require('att');
+
+//Note: Application Developer should get access key and secret key by registering on www.developer.att.com website
+//and creating sample Application.
+// <module platform="commonjs" version="1.0.1">att</module>
+var accessKey = Ti.App.Properties.getString('accessKey');
+var secretKey = Ti.App.Properties.getString('secretKey');
+var scope = Ti.App.Properties.getString('scope');
+var grantType = 'client_credentials';
+var isAndroid = Titanium.Platform.osname === "android";
+
+//pass accessKey and secretKey for authorization
+attAPIs.ATT.authorize(accessKey, secretKey, scope, grantType);
+
+//Helper functions and variables
+var util = require('util'),
+    forEach = util.forEach,
+    copyObjTo = util.copyObjTo,
+    createPicker = util.UI.createPicker,
+    deviceState = util.state.device,
+    isAndroid = deviceState.is.android,
+    openPopUp = util.UI.createResponseWindow();
+
+var ttsConstants = {
+    margin: 10
+};
+
+//Set up main window
+var mainWindow = Ti.UI.currentWindow;
+mainWindow.layout = 'vertical';
+
+mainWindow.addEventListener('android:back', function(e) {
+    mainWindow.close();
+});
+
+var scrollView = Ti.UI.createScrollView({
+    top: 0,
+    layout:'vertical',
+    height: Ti.UI.SIZE,
+    left: ttsConstants.margin, 
+    right: ttsConstants.margin,
+});
+mainWindow.add(scrollView);
+
+//Create textarea to type text
+var inputTextArea = Ti.UI.createTextArea({
+    width: '100%',
+    height : 100,
+    color : 'black',
+    borderColor : 'black',
+    font: {
+        fontSize: deviceState.is.android ? 18 : 16
+    },
+    hintText: 'Enter your text here'
+});
+scrollView.add(util.UI.createLabelView('Text', inputTextArea, { top : ttsConstants.margin }));
+
+var pickerOptions = {
+    borderColor : 'black'
+};
+
+var languagePickerView = util.UI.createPicker(pickerOptions, ['', 'en-US', 'es-US']);
+scrollView.add(util.UI.createLabelView('Content Language', languagePickerView));
+
+var voicePickerWrapper = Ti.UI.createView({
+    height: Ti.UI.SIZE
+});
+scrollView.add(voicePickerWrapper);
+
+var voicePickerEnglish = util.UI.createPicker(pickerOptions, ['', 'crystal', 'mike']);
+var voicePickerEnglishRow = util.UI.createLabelView('English Voice', voicePickerEnglish, {top: 0});
+voicePickerWrapper.add(voicePickerEnglishRow);
+
+var voicePickerSpanish = util.UI.createPicker(pickerOptions, ['rosa', 'alberto']);
+var voicePickerSpanishRow = util.UI.createLabelView('Spanish Voice', voicePickerSpanish, {top: 0});
+voicePickerWrapper.add(voicePickerSpanishRow);
+voicePickerSpanishRow.visible = false;
+
+languagePickerView.addEventListener('change', function(e) {
+    var isSpanish = languagePickerView.value === 'es-US';
+    
+    voicePickerEnglishRow.visible = !isSpanish;
+    voicePickerSpanishRow.visible = isSpanish;
+});
+
+
+//Create submit button
+var sendTextButton = Ti.UI.createButton({
+    title : 'Send Text',
+    top : 20,
+    height : 40,
+});
+
+var playSoundButton;
+function createPlaySoundButton(filePath) {
+    var player = Ti.Media.createSound({url:filePath});
+    
+    if(playSoundButton) scrollView.remove(playSoundButton);
+    
+    playSoundButton = Ti.UI.createButton({
+        title : 'Play Sound',
+        top : 10,
+        height : 40
+    });
+    
+    playSoundButton.addEventListener('click', function() {
+        if(!player.playing) player.play();
+    });
+    
+    scrollView.add(playSoundButton);
+}
+
+function getTextToSpeech() {
+    var text = inputTextArea.value;
+    Ti.API.info('Text entered: ' + text);
+    
+    if(!text) {
+        alert('Enter text to convert to speech');
+        return;
+    }
+    
+    /**
+     * 
+     * @param options.contentType- format in which data is to be sent, either "text/plain" or "application/ssml+xml"
+     * @param options.accept - The format of the audio returned, either "audio/amr", "audio/amr-wb", or "audio/wav"
+     * @param options.filePath - The path where to save the audio. It will write over any existing file. It appends 
+     *                   the proper extension (based on the accept) if one is not added
+     * @param options.body- Will contain the plain text or SSML to convert to speech
+     * 
+     **/
+    var fileName = 'tts';
+    var filePath = Ti.Filesystem[isAndroid ? 'externalStorageDirectory' : 'applicationDataDirectory'] + fileName;
+       
+    attAPIs.ATT.Speech.textToSpeech({
+        'accept' : 'audio/x-wav',
+        'contentType' : 'text/plain',
+        'body' : text,
+        'filePath': filePath,
+        'contentLanguage': languagePickerView.value,
+        'xArg': {
+            VoiceName: (languagePickerView.value === 'es-US') ? voicePickerSpanish.value : voicePickerEnglish.value
+        }
+    }, function(data) {
+        //Ti.API.log(data);
+        createPlaySoundButton(JSON.parse(data).filePath);
+    }, function(error) {
+        Ti.API.error('Error Callback:' + error);
+        openPopUp(error, true);
+    });
+}
+
+//On Button Click user call method 'SendSMS'
+sendTextButton.addEventListener('click', function() {"use strict";
+    Ti.API.info('Get TTS Button Clicked.');
+    
+    inputTextArea.blur();
+    getTextToSpeech();
+});
+
+scrollView.add(sendTextButton);
